1cca4e9475765c2a067dc18aa972d8c217bc049f
beta9 <egottli2@binghamton.edu>
2024-06-06 19:14:25 -0400

    add secrets example

--- 02_customizing_environment/using_secrets.py
+++ 02_customizing_environment/using_secrets.py
@@ -1,13 +1,23 @@
 """
-Warning: This is not implemented yet!
+Managing Secrets and Environment Variables in Beam
+
+You can read and write secrets that will be available to your apps.
+
+To create a secret, use the Beam CLI: `beam secret create [KEY] [VALUE]`
+
+Once the secret is created, it can be accessed as an environment variable (see below).
 """
 
 from beam import function
-import os
-
-MY_SECRET = os.environ["MY_SECRET"]
 
 
-@function()
+@function(secrets=["AWS_ACCESS_KEY"])
 def handler():
-    print(f"Secret: {MY_SECRET}")
+    import os
+
+    my_secret = os.environ["AWS_ACCESS_KEY"]
+    print(f"Secret: {my_secret}")
+
+
+if __name__ == "__main__":
+    handler()
