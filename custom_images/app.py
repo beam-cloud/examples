66cf5cceffe82bea645e61e2de6d8c70f1fbd9aa
Eli Mernit <egottli2@binghamton.edu>
2024-09-12 19:29:02 -0400

    Eli/update jupyter and images (#39)
    * update quickstart
    
    * update image example and add jupyter example
    
    * clean up
    
    * remove ipynb checkpoints

--- custom_images/app.py
+++ custom_images/app.py
@@ -1,13 +1,13 @@
 from beam import endpoint, Image
 
 
-image = Image(
-    python_version="python3.9",
-    python_packages=[
-        "torch",
-    ],
-    commands=["apt-get update -y && apt-get install neovim -y"],
-    base_image="docker.io/nvidia/cuda:12.3.1-runtime-ubuntu20.04",
+image = (
+    Image(
+        base_image="docker.io/nvidia/cuda:12.3.1-runtime-ubuntu20.04",
+        python_version="python3.9",
+    )
+    .add_commands(["apt-get update -y", "apt-get install neovim -y"])
+    .add_python_packages(["torch"])
 )
 
 
