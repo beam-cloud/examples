61a5fcafe2b52ce37deaef7ee015cfa4158a3b14
John <john@slai.io>
2024-10-02 10:37:31 -0700

    Remove remote call in quickstarT

--- quickstart/app.py
+++ quickstart/app.py
@@ -4,8 +4,4 @@ from beam import function, Image
 @function(image=Image(python_packages=["numpy"]))
 def hello_world():
     import numpy
-
     return "I'm running in a remote container!"
-
-
-print(hello_world.remote())
